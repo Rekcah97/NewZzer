e9024ef4c248433c2a148d7a52ea2217375e9c79
unknown <arpitregmi121@gmail.com>
2024-08-07 19:29:12 +0545

    top loading bar

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   static defaultProps = {
@@ -21,68 +22,66 @@ export class News extends Component {
     super(props);
     this.state = {
       article: [],
-      loading: false,
+      loading: true,
       page: 1,
+      totalResults: 0,
     };
     document.title = `${this.capatalizeFirstLetter(this.props.category)} - Newzzer`;
   }
 
   async updateNews() {
-    const url = `https://gnews.io/api/v4/top-headlines?country=${this.props.country}&category=${this.props.category}&max=100&apikey=49a83d33f62c7cd75f017685b73cbc9d`;
-    //const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a5ff1698c0b4472d9718777851313c99&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.props.setProgress(10);
+    //  const url = `https://gnews.io/api/v4/top-headlines?country=${this.props.country}&category=${this.props.category}&max=100&apikey=49a83d33f62c7cd75f017685b73cbc9d`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
 
     let data = await fetch(url);
+    this.props.setProgress(30);
 
     let parsedData = await data.json();
-    this.setState({ loading: false });
+    this.props.setProgress(50);
 
-    this.setState({ article: parsedData.articles, totalResults: parsedData.totalResults });
+    this.setState({ article: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
     this.updateNews();
   }
 
-  handlePreviousClick = async () => {
-    this.setState({
-      page: this.state.page - 1,
-    });
-    this.updateNews();
-  };
-  handleNextClick = async () => {
+  fetchMoreData = async () => {
     this.setState({
       page: this.state.page + 1,
     });
-    this.updateNews();
+    // const url = `https://gnews.io/api/v4/top-headlines?country=${this.props.country}&category=${this.props.category}&max=100&apikey=49a83d33f62c7cd75f017685b73cbc9d`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a5ff1698c0b4472d9718777851313c99&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+
+    let data = await fetch(url);
+
+    let parsedData = await data.json();
+
+    this.setState({ article: this.state.article.concat(parsedData.articles), totalResults: parsedData.totalResults });
   };
 
   render() {
     return (
       <>
-        <div className="container my-3">
-          {/* heading */}
-          <h2 className="text-center">Newzzer - Top {this.capatalizeFirstLetter(this.props.category)} Headline </h2>
-
-          {this.state.loading && <Spinner />}
-          <div className="row">
-            {this.state.article.map((element) => {
-              return (
-                <div className="col-md-4 my-3" key={element.url}>
-                  <NewsItem title={element.title ? element.title : ""} source={element.source.name} author={element.author} time={element.publishedAt} description={element.description ? element.description : ""} imgUrl={element.urlToImage} newsUrl={element.url} />
-                </div>
-              );
-            })}
+        {/* heading */}
+        <h2 className="text-center my-3">Newzzer - Top {this.capatalizeFirstLetter(this.props.category)} Headline </h2>
+        {this.state.loading && <Spinner />}
+        <InfiniteScroll dataLength={this.state.article.length} next={this.fetchMoreData} hasMore={this.state.article.length !== this.state.totalResults && this.state.article.length <= this.state.totalResults} loader={<Spinner />}>
+          <div className="container">
+            <div className="row">
+              {this.state.article.map((element) => {
+                return (
+                  <div className="col-md-4 my-3" key={element.url}>
+                    <NewsItem key={element.id} title={element.title ? element.title : ""} source={element.source.name} author={element.author} time={element.publishedAt} description={element.description ? element.description : ""} imgUrl={element.urlToImage} newsUrl={element.url} />
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>
-        <div className="container d-flex justify-content-between my-2">
-          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>
-            &larr; Previous
-          </button>
-          <button disabled={this.state.page + 1 >= Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>
-            Next &rarr;
-          </button>
-        </div>
+        </InfiniteScroll>
       </>
     );
   }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import loading from "./loader2.gif";
 export default function Spinner() {
   return (
     <div className="text-center">
-      <img src={loading} alt="loading" />
+      <img className="my-3" src={loading} alt="loading" />
     </div>
   );
 }
